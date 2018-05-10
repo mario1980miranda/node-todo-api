61b883a67d97465231227a0f493102c5a0132897
mario_chan15 <mario.luiz.miranda@gmail.com>
2018-05-10 12:52:22 -0300

    S07C71 - Validators, Types and Defaults

--- server/server.js
+++ server/server.js
@@ -3,15 +3,49 @@ var mongoose = require('mongoose');
 mongoose.Primse = global.Promise;
 mongoose.connect('mongodb://localhost:27017/TodoApp');
 
-var Todo = mongoose.model('Todo', {
-    text: {
-        type: String
-    },
-    completed: {
-        type: Boolean
-    },
-    completedAt: {
-        type: Number
+// var Todo = mongoose.model('Todo', {
+//     text: {
+//         type: String,
+//         required: true,
+//         minlength: 1,
+//         trim: true
+//     },
+//     completed: {
+//         type: Boolean,
+//         default: false
+//     },
+//     completedAt: {
+//         type: Number,
+//         default: null
+//     }
+// });
+
+var User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
+    }, userName: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
+    }, password: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
+    }, location: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
+    }, email: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
     }
 });
 
@@ -25,14 +59,28 @@ var Todo = mongoose.model('Todo', {
 //     console.log(e);
 // });
 
-var newTodoTwo = new Todo({
-    text: 'Defeat Micolash, Host of the nightmare',
-    completed: true,
-    completedAt: 10052018
+// var newTodoTwo = new Todo({
+//     text: 'Defeat Micolash, Host of the nightmare',
+//     completed: true,
+//     completedAt: 10052018
+// });
+
+// newTodoTwo.save().then((doc) => {
+//     console.log('Saved todo', doc);
+// }, (e) => {
+//     console.log(e);
+// });
+
+var newUser = new User({
+    name: 'Mario Miranda',
+    userName: 'mario_chan15',
+    password: 'admin123',
+    location: 'Curitiba',
+    email: 'mario_chan15@hotmail.com'
 });
 
-newTodoTwo.save().then((doc) => {
-    console.log('Saved todo', doc);
+newUser.save().then((doc) => {
+    console.log('Saved user', doc);
 }, (e) => {
     console.log(e);
 });
